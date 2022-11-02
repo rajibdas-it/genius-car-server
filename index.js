8a3c6c167c9d633831a9a2c7f3c972bd1b25d9b8
rajibdas-it <rajibrad@gmail.com>
2022-11-02 17:35:42 +0600

    order placed in db functtion done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -15,12 +15,24 @@ async function run() {
   const uri = "mongodb://localhost:27017";
   const client = new MongoClient(uri);
   const servicesCollection = client.db("geniusCar").collection("services");
+  const orderCollection = client.db("geniusCar").collection("orders");
   try {
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = servicesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/services", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      console.log(result);
     });
   } finally {
   }
